1f36d4cecc0a208421499a139cc43f29c301d6da
Pedro Knoll <p.giacometo@gmail.com>
2018-08-15 13:41:36 -0300

    test: Add tests cases to python solution
        Test cases:
        1. test_search_with_ordered_array:
             > doSearch params: Unordered array, valid targetNum
             > assertEqual params: correct resultNum param
        2. test_search_with_unordered_array
             > doSearch params: Ordered array, valid targetNum
             > assertEqual params: correct resultNum param
        3. test_search_with_invalid_target_num
             > doSearch params: Ordered array, invalid targetNum
             > assertEqual params: correct resultNum param

--- binary-search/prime-finder/solution.py
+++ binary-search/prime-finder/solution.py
@@ -1,3 +1,5 @@
+import unittest
+
 # Do the loop as long as be possible the number is within the array.
 def do_search(nums_array, target_num):
     min_index = 0;
@@ -13,3 +15,24 @@ def do_search(nums_array, target_num):
         else:
             max_index = guess_index - 1
     return -1
+
+# Test Cases
+class TestDoSearch(unittest.TestCase):
+
+    def test_search_with_ordered_array(self):
+        ordered_array = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47]
+        search = do_search(ordered_array, 13)
+        self.assertEqual(search, 5)
+
+    def test_search_with_unordered_array(self):
+        unordered_array = [13,2,3,5,7,11,17,19,23,29,31,37,41,43,47]
+        search = do_search(unordered_array, 13)
+        self.assertEqual(search,5)
+
+    def test_search_with_invalid_target_num(self):
+        prime_nums = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47]
+        search = do_search(prime_nums, 12)
+        self.assertEqual(search, -1)
+
+if __name__ == '__main__':
+    unittest.main()
