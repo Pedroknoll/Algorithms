9ee949a0dc167440a69462d60e3da312e5dda368
Pedro Knoll <p.giacometo@gmail.com>
2018-08-16 20:49:08 -0300

    feat: Create a function to find the minimum value index at an array
    The function receive as params an array and the start index of the
    subarray that you want to find the index of the minimum value.

--- sort/selection-sort/solution.py
+++ sort/selection-sort/solution.py
@@ -14,6 +14,25 @@ def swap_elements(array, first_index, second_index):
     return array
 
 
+def min_value_index(array, start_index):
+    """Return the index of the minimum value into a subarray
+
+    Arguments:
+    array -- the target array
+    start_index -- the first index of the subarray
+    """
+    min_index = start_index
+    min_value = array[start_index]
+
+    next_index = min_index + 1
+    while next_index < len(array):
+        if array[next_index] < min_value:
+            min_index = next_index
+            min_value = array[next_index]
+        next_index += 1
+    print(min_index)
+    return min_index
+
 
 
 class TestSwapElement(unittest.TestCase):
