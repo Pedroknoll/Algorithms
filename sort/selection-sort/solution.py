4c917c688edb326a179e993692c7560df4d238b3
Pedro Knoll <p.giacometo@gmail.com>
2018-08-16 19:39:43 -0300

    test: Create test case for swap elements with valid params

--- sort/selection-sort/solution.py
+++ sort/selection-sort/solution.py
@@ -1,3 +1,5 @@
+import unittest
+
 def swap_elements(array, first_index, second_index):
     """Swaps the positions between elements of a given array
 
@@ -9,3 +11,18 @@ def swap_elements(array, first_index, second_index):
     temp = array[first_index]
     array[first_index] = array[second_index]
     array[second_index] = temp
+    return array
+
+
+
+
+class TestSwapElement(unittest.TestCase):
+
+    def test_swap_elements_in_array(self):
+        test_array = [10,3,2,6]
+        swaped_array = swap_elements(test_array,0, 1)
+        self.assertEqual(swaped_array, [3,10,2,6])
+
+
+if __name__ == '__main__':
+    unittest.main()
